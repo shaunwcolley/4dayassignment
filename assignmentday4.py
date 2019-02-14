360c5a2a3a6a5b5bf4d20614ded01b9123508164
Shaun W Colley <swc7838@gmail.com>
2019-02-14 14:42:36 -0600

    Finished app, still missing ability to recognize input that is not a number

--- assignmentday4.py
+++ assignmentday4.py
@@ -9,25 +9,38 @@ tasks = []
 
 def add_task():
     task = {}
-    title = input("Please enter a task: \n")
-    priorty = input("High, med, or low priorty?\n")
+    title = input("Please enter a task: \n>> ")
+    priorty = input("High, med, or low priorty?\n>> ")
     task["title"] = title
     task["priority"] = priorty
     tasks.append(task)
 
 def view_tasks():
     for task in tasks:
+        print(f"{tasks.index(task) + 1}", end=" ")
         for key in task.keys():
-            print(task[key])
+            print(" - ", task[key], end=" ")
+        print("\n")
+
+def del_task():
+    condition = True
+    while condition == True:
+        del_input = input("Which number task did you wish to remove?\n>> ")
+        if del_input <= len(tasks):
+            del tasks[(del_input - 1)]
+            condition = False
+        else:
+            print(f"Please give a up to {str(len(tasks) + 1)}, thank you!")
 
 user_input = ""
+
 while user_input != "q":
     display_menu()
     user_input = input(">> ")
     if user_input == "1":
         add_task()
     elif user_input == "2":
-        print("2")#del_task()
+        del_task()
     elif user_input == "3":
         view_tasks()
     elif user_input != "q":
